3365112b4154f33f1c88094c72016005b3140123
John D. Haughton <AnotherJohnH@users.noreply.github.com>
2017-05-19 21:58:10 +0100

    Clean up code in ZLauncher

--- Game/zif/ZLauncher.h
+++ Game/zif/ZLauncher.h
@@ -39,13 +39,14 @@ private:
    PLT::Device&   term;
    const char*    config_file{nullptr};
    unsigned       cursor{0};
-   unsigned       limit{0};
-   bool           selection_is_dir{false};
+   unsigned       cursor_limit{0};
    bool           quit{false};
+   char           path[FILENAME_MAX] = {};
    char           selection[FILENAME_MAX] = {};
-   char           prefix[FILENAME_MAX] = {};
+   bool           selection_is_dir{false};
 
-   bool getLine(FILE* fp, char* file, size_t size)
+   //! Get the next line with content from the given file stream
+   static bool getLine(FILE* fp, char* file, size_t size)
    {
       while(fgets(file, size, fp))
       {
@@ -60,6 +61,7 @@ private:
       return false;
    }
 
+   //! 
    void drawHeader()
    {
       clear();
@@ -71,57 +73,68 @@ private:
       attroff(PLT::A_REVERSE);
 
       attron(PLT::A_BOLD);
-      mvaddstr(3, 3, prefix);
+      mvaddstr(3, 3, path);
       attroff(PLT::A_BOLD);
    }
 
+   //! 
    void drawList()
    {
-      char prev[FILENAME_MAX];
-      prev[0] = '\0';
-
       FILE* fp = fopen(config_file, "r");
       if (fp == nullptr)
       {
          return;
       }
 
-      for(unsigned line =0; line<lines; )
-      {
-         char text[FILENAME_MAX];
+      char prev[FILENAME_MAX];
+      prev[0] = '\0';
 
-         if (!getLine(fp, text, sizeof(text)))
+      const unsigned first_row = 4;
+
+      for(unsigned index = 0; (first_row + index)<lines; )
+      {
+         // Read one line from the config gile
+         char line[FILENAME_MAX];
+         if (!getLine(fp, line, sizeof(line)))
          {
-            limit = line - 1;
+            cursor_limit = index - 1;
             break;
          }
 
-         if (strncmp(text, prefix, strlen(prefix)) == 0)
+         // Does the start of the line match the current path
+         if (strncmp(line, path, strlen(path)) == 0)
          {
-            // Start of line matches current prefix
-            char* suffix = text + strlen(prefix);
+            char* entry = line + strlen(path);
 
+            // Extract entry and check if it is a directory
             bool is_dir = false;
-            char* s = strchr(suffix, '/');
-            if (s != nullptr)
+            char* slash = strchr(entry, '/');
+            if (slash != nullptr)
             {
-               *s = '\0';
+               *slash = '\0';
                is_dir = true;
             }
 
-            if (strcmp(suffix, prev) != 0)
+            // Is this entry different to the previous entry
+            if (strcmp(entry, prev) != 0)
             {
-               strcpy(prev, suffix);
+               strcpy(prev, entry);
 
-               if (line++ == cursor)
+               // Is this the currently selected entry
+               if (index++ == cursor)
                {
-                  strcpy(selection, suffix);
+                  strcpy(selection, entry);
                   selection_is_dir = is_dir;
 
                   attron(PLT::A_REVERSE);
                }
 
-               mvaddstr(4 + line, 3, suffix[0] == '!' ? suffix + 1 : suffix);
+               if (entry[0] == '!')
+               {
+                  entry++;
+               }
+
+               mvaddstr(first_row + index, 3, entry);
 
                attroff(PLT::A_REVERSE);
             }
@@ -129,34 +142,6 @@ private:
       }
 
       fclose(fp);
-   }
-
-   void openDir()
-   {
-      cursor = 0;
-      strcat(prefix, selection);
-      strcat(prefix, "/");
-   }
-
-   void closeDir()
-   {
-      char* s = strrchr(prefix, '/');
-      if (s)
-      {
-         *s = '\0';
-
-         s = strrchr(prefix, '/');
-         if (s)
-         {
-            s[1] = '\0';
-         }
-         else
-         {
-            prefix[0] = '\0';
-         }
-
-         cursor = 0;
-      }
    }
 
    void layoutText(unsigned l, unsigned c, const char* text)
@@ -199,6 +184,7 @@ private:
       }
    }
 
+   //! Display info page
    void doInfo()
    {
       drawHeader();
@@ -219,59 +205,113 @@ private:
       (void) getch();
    }
 
-   void action(const char* cmd)
+   //! Implement an action
+   void doAction(const char* cmd, const char* value = "")
    {
+      if (strcmp(cmd, "Quit") == 0)
+      {
+         quit = true;
+      }
+      else if (strcmp(cmd, "Info") == 0)
+      {
+         doInfo();
+      }
+      else if (strcmp(cmd, "Video") == 0)
+      {
 #ifdef PROJ_TARGET_Kindle3
-      const uint32_t dark  = 0x000000;
-      const uint32_t light = 0xFFFFFF;
+         const uint32_t dark  = 0x000000;
+         const uint32_t light = 0xFFFFFF;
 #else
-      const uint32_t dark  = 0x382800;
-      const uint32_t light = 0xF0F0E0;
+         const uint32_t dark  = 0x382800;
+         const uint32_t light = 0xF0F0E0;
 #endif
 
-      if (cmd[0] == '!')
-      {
-         ++cmd;
-
-         if (strcmp(cmd,  "QUIT") == 0)
-         {
-            quit = true;
-         }
-         else if (strcmp(cmd,  "INFO") == 0)
-         {
-            doInfo();
-         }
-         else if (strcmp(cmd,  "Inverse") == 0)
+         if (strcmp(value, "Inverse") == 0)
          {
             term.ioctl(PLT::Device::IOCTL_TERM_PALETTE, 0, light);
             term.ioctl(PLT::Device::IOCTL_TERM_PALETTE, 7, dark);
          }
-         else if (strcmp(cmd,  "Normal") == 0)
+         else if (strcmp(value,  "Normal") == 0)
          {
             term.ioctl(PLT::Device::IOCTL_TERM_PALETTE, 0, dark);
             term.ioctl(PLT::Device::IOCTL_TERM_PALETTE, 7, light);
          }
-         else if (strncmp(cmd, "Border=", 7) == 0)
-         {
-            term.ioctl(PLT::Device::IOCTL_TERM_BORDER, atoi(cmd + 7));
-         }
-         else if (strncmp(cmd, "LineSpace=", 10) == 0)
-         {
-            term.ioctl(PLT::Device::IOCTL_TERM_LINE_SPACE, atoi(cmd + 10));
-         }
-         else if (strncmp(cmd, "FontSize=", 9) == 0)
-         {
-            term.ioctl(PLT::Device::IOCTL_TERM_FONT_SIZE, atoi(cmd + 9));
+      }
+      else if (strcmp(cmd, "Border") == 0)
+      {
+         term.ioctl(PLT::Device::IOCTL_TERM_BORDER, atoi(value));
+         Curses::init();
+      }
+      else if (strcmp(cmd, "LineSpace") == 0)
+      {
+         term.ioctl(PLT::Device::IOCTL_TERM_LINE_SPACE, atoi(value));
+         Curses::init();
+      }
+      else if (strcmp(cmd, "FontSize") == 0)
+      {
+         term.ioctl(PLT::Device::IOCTL_TERM_FONT_SIZE, atoi(value));
+         Curses::init();
+      }
+   }
 
-            Curses::init();
+
+   //! Open a config directory (not a real directory)
+   void openDir(const char* sub_directory)
+   {
+      strcat(path, sub_directory);
+      strcat(path, "/");
+      cursor = 0;
+   }
+
+   //! Close a config directory (not a real directory)
+   void closeDir()
+   {
+      char* s = strrchr(path, '/');
+      if (s)
+      {
+         *s = '\0';
+
+         s = strrchr(path, '/');
+         if (s)
+         {
+            s[1] = '\0';
+         }
+         else
+         {
+            path[0] = '\0';
+         }
+
+         cursor = 0;
+      }
+   }
+
+   //! Select
+   void doSelect(char* selection)
+   {
+      if (selection[0] == '!')
+      {
+         char* cmd = selection + 1;
+
+         char* value = strchr(cmd, '=');
+         if (value)
+         {
+            *value = '\0';
+
+            doAction(cmd, value + 1);
+         }
+         else
+         {
+            doAction(cmd);
          }
       }
       else
       {
+         // Load and run a game
+
          char story[FILENAME_MAX];
 
-         strcpy(story, prefix);
-         strcat(story, cmd);
+         strcpy(story, path);
+         strcat(story, selection);
 
          run(story);
       }
@@ -283,18 +323,20 @@ public:
       , term(term_)
       , config_file(config_file_)
    {
-      action("!Border=0");
-      action("!FontSize=18");
-      action("!LineSpace=1");
-      action("!Inverse");
+      doAction("Border",    "0");
+      doAction("FontSize",  "18");
+      doAction("LineSpace", "1");
+      doAction("Video",     "Inverse");
    }
 
+   //! Load and run a story file
    int run(const char* story)
    {
       ZMachine(&term).open(story);
       return 0;
    }
 
+   //! Enter the menu system
    int menu()
    {
       while(!quit)
@@ -307,15 +349,15 @@ public:
          switch(key)
          {
          case PLT::ESCAPE:
-            action("!QUIT");
+            doAction("Quit");
             break;
 
          case PLT::UP:
-            cursor = cursor == 0 ? limit : cursor - 1;
+            cursor = cursor == 0 ? cursor_limit : cursor - 1;
             break;
 
          case PLT::DOWN:
-            cursor = cursor == limit ? 0 : cursor + 1;
+            cursor = cursor == cursor_limit ? 0 : cursor + 1;
             break;
 
          case '\n':
@@ -324,11 +366,11 @@ public:
          case PLT::PAGE_DOWN:
             if (selection_is_dir)
             {
-               openDir();
+               openDir(selection);
             }
             else
             {
-               action(selection);
+               doSelect(selection);
             }
             break;
 
@@ -338,7 +380,10 @@ public:
             break;
 
          default:
-            quit = (key < 0);
+            if (key < 0)
+            {
+               doAction("Quit");
+            }
             break;
          }
       }
