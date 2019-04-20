48e772d9b818eb12da0ef73ff0bd1920a75e9214
John D. Haughton <AnotherJohnH@users.noreply.github.com>
2019-04-20 19:15:04 +0100

    Info page updates

--- Source/launcher/InfoPage.h
+++ Source/launcher/InfoPage.h
@@ -53,24 +53,25 @@ public:
 
       curses.mvaddstr(3, 3, "Program     : "); curses.addstr(program.c_str());
       curses.mvaddstr(4, 3, "Description : "); curses.addstr(description.c_str());
-      curses.mvaddstr(5, 3, "Link        : "); if(link != "") curses.addstr(link.c_str());
-      curses.mvaddstr(6, 3, "Author      : "); curses.addstr(author.c_str());
-      curses.mvaddstr(7, 3, "Version     : "); curses.addstr(version.c_str());
+      curses.mvaddstr(5, 3, "Author      : "); curses.addstr(author.c_str());
+      curses.mvaddstr(6, 3, "Version     : "); curses.addstr(version.c_str());
+      curses.mvaddstr(7, 3, "Link        : "); if(link != "") curses.addstr(link.c_str());
 
-      curses.mvaddstr(8, 3, "Built       : ");
+      curses.mvaddstr(8, 3, "Commit      : "); curses.addstr(PROJ_COMMIT);
+      curses.mvaddstr(9, 3, "Built       : ");
       curses.addstr(__TIME__); curses.addstr(" "); curses.addstr(__DATE__);
 
-      curses.mvaddstr(9, 3, "Compiler    : "); layoutText(9, 17, __VERSION__);
+      curses.mvaddstr(10, 3, "Compiler    : "); layoutText(10, 17, __VERSION__);
 
       curses.attron(TRM::A_BOLD);
-      curses.mvaddstr(11, 3, "Copyright (c) ");
+      curses.mvaddstr(12, 3, "Copyright (c) ");
       curses.addstr(copyright_year.c_str());
       curses.addstr(" ");
       curses.addstr(author.c_str());
 
       curses.attroff(TRM::A_BOLD);
 
-      layoutText(13, 3, MIT_LICENSE);
+      layoutText(14, 3, MIT_LICENSE);
 
       return false;
    }
